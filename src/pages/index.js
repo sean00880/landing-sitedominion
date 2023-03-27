934c054f042654baa2e7c9499e32bdc28321cd0f
sean00880 <theelitereapersanctum@gmail.com>
2023-03-26 21:38:39 -0400

    2nd commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,68 +1,66 @@
 import React, { Fragment } from 'react';
 import Head from 'next/head';
-import Sticky from 'react-stickynode';
 import { ThemeProvider } from 'styled-components';
-import { agencyTheme } from 'common/theme/agency';
-import ResetCSS from 'common/assets/css/style';
-import { GlobalStyle, AgencyWrapper } from 'containers/Agency/agency.style';
-import Navbar from 'containers/Agency/Navbar';
-import BannerSection from 'containers/Agency/BannerSection';
-import FeatureSection from 'containers/Agency/FeatureSection';
-import AboutUsSection from 'containers/Agency/AboutUsSection';
-import WorkHistory from 'containers/Agency/WorkHistory';
-import BlogSection from 'containers/Agency/BlogSection';
-import TestimonialSection from 'containers/Agency/TestimonialSection';
-import TeamSection from 'containers/Agency/TeamSection';
-import VideoSection from 'containers/Agency/VideoSection';
-import NewsletterSection from 'containers/Agency/NewsletterSection';
-import QualitySection from 'containers/Agency/QualitySection';
-import Footer from 'containers/Agency/Footer';
+import Sticky from 'react-stickynode';
 import { DrawerProvider } from 'common/contexts/DrawerContext';
-import FaqSection from 'containers/Agency/FaqSection';
+import { saasModernTheme } from 'common/theme/saasModern';
+import ResetCSS from 'common/assets/css/style';
+import { GlobalStyle, ContentWrapper } from 'containers/SaasModern/sassModern.style';
 
-const Main = () => {
-	return (
-		<ThemeProvider theme={agencyTheme}>
-			<Fragment>
-				{/* Start agency head section */}
-				<Head>
-					<title>Agency | A react next landing page</title>
-					<meta name="theme-color" content="#10ac84" />
-					<meta name="Description" content="React next landing page" />
-					{/* Load google fonts */}
-					<link
-						href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i"
-						rel="stylesheet"
-					/>
-				</Head>
-				<ResetCSS />
-				<GlobalStyle />
-				{/* End of agency head section */}
-				{/* Start agency wrapper section */}
+import BannerSection from 'containers/SaasModern/Banner';
+import Navbar from 'containers/SaasModern/Navbar';
+import WorkingProcessSection from 'containers/SaasModern/WorkingProcess';
+import PricingSection from 'containers/SaasModern/Pricing';
+import PartnerSection from 'containers/SaasModern/Partner';
+import FaqSection from 'containers/SaasModern/Faq';
+import TrialSection from 'containers/SaasModern/Trial';
+import InfoSection from 'containers/SaasModern/Info';
+import FeatureSection from 'containers/SaasModern/Feature';
+import UpdateScreen from 'containers/SaasModern/UpdateScreen';
+import TestimonialSection from 'containers/SaasModern/Testimonial';
+import Footer from 'containers/SaasModern/Footer';
+import Service from 'containers/AgencyDigital/Service';
+import Gallery from 'containers/Interior/Gallery';
 
-				<AgencyWrapper>
-					<Sticky top={0} innerZ={9999} activeClass="sticky-nav-active">
-						<DrawerProvider>
-							<Navbar />
-						</DrawerProvider>
-					</Sticky>
-					<BannerSection />
-					<FeatureSection />
-					<AboutUsSection />
-					<WorkHistory />
-					<BlogSection />
-					<QualitySection />
-					<VideoSection />
-					<TestimonialSection />
-					<TeamSection />
-					<FaqSection />
-					<NewsletterSection />
-					<Footer />
-				</AgencyWrapper>
+const SaasModern = () => {
+  return (
+    <ThemeProvider theme={saasModernTheme}>
+      <Fragment>
+        <Head>
+          <title>Site Dominion | Welcome</title>
+          <meta name="Description" content="React next landing page" />
+          <meta name="theme-color" content="#ec5555" />
+          {/* Load google fonts */}
+          <link
+            href="https://fonts.googleapis.com/css?family=Roboto:300,400,400i,500,500i,700,900|Open+Sans:400,400i,600,700"
+            rel="stylesheet"
+          />
+        </Head>
 
-				{/* End of agency wrapper section */}
-			</Fragment>
-		</ThemeProvider>
-	);
+        <ResetCSS />
+        <GlobalStyle />
+
+        <ContentWrapper>
+          <Sticky top={0} innerZ={9999} activeClass="sticky-nav-active">
+            <DrawerProvider>
+              <Navbar />
+            </DrawerProvider>
+          </Sticky>
+          <BannerSection />
+          <InfoSection />
+          <WorkingProcessSection />
+          
+          <FeatureSection />
+          <UpdateScreen />
+          <PricingSection />
+          <Gallery/>
+          <TestimonialSection />
+          <FaqSection />
+          <TrialSection />
+          <Footer />
+        </ContentWrapper>
+      </Fragment>
+    </ThemeProvider>
+  );
 };
-export default Main;
+export default SaasModern;
