b9bee920335e3dcb1266146c98b303d02f050785
sean00880 <theelitereapersanctum@gmail.com>
2023-04-05 03:02:22 -0400

    11th commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -9,10 +9,26 @@ import 'common/assets/css/react-slick.css';
 import 'common/assets/css/rc-collapse.css';
 import 'rc-collapse/assets/index.css';
 import 'common/assets/css/rc-drawer.css';
+import Head from 'next/head';
+import Script from 'next/script';
 
 export default function CustomApp({ Component, pageProps }) {
   return (
     <Modal>
+       <Head>
+            <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-LYS6SMLR0H');
+        `}
+        
+      </Script>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0" />
+      
+            </Head>
       <Component {...pageProps} />
     </Modal>
   );
