736a74bf3c4a5bb9f94dba6f7ed2f981fbcd55c2
sean00880 <theelitereapersanctum@gmail.com>
2023-04-12 21:34:04 -0400

    19th commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -9,32 +9,49 @@ import 'common/assets/css/react-slick.css';
 import 'common/assets/css/rc-collapse.css';
 import 'rc-collapse/assets/index.css';
 import 'common/assets/css/rc-drawer.css';
-import Head from 'next/head';
-import Script from 'next/script';
 
-export default function CustomApp({ Component, pageProps }) {
+import Head from 'next/head'
+import { useRouter } from 'next/router'
+import Script from 'next/script'
+import { useEffect } from 'react'
+import * as gtag from '../lib/gtag'
+
+const App = ({ Component, pageProps }) => {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return (
     <Modal>
-       <Head>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0" />
-      
-            </Head>
-            <Script src="https://www.googletagmanager.com/gtag/js?id=G-LYS6SMLR0H"
-        strategy="afterInteractive"></Script>
-            <Script
-              id='google-analytics'
-              strategy="afterInteractive"
-              dangerouslySetInnerHTML={{
-              __html: `
+      <Head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', 'G-LYS6SMLR0H', {
+              gtag('config', '${gtag.GA_TRACKING_ID}', {
                 page_path: window.location.pathname,
               });
-              `,
-              }}/>
+            `,
+          }}
+        />
+      </Head>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
       <Component {...pageProps} />
     </Modal>
-  );
+  )
 }
+
+export default App
